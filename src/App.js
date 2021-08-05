49ad985e4e41b1a3a652c6f6638b5e8a91adf8da
Jsesan <jsesan00@gmail.com>
2021-08-05 10:13:34 -0300

    Cart and Modal State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+
+  const[openCart, setOpenCart] = useState(false);
+
+  const openCartHandler = () => {
+    setOpenCart(true);
+  }
+
+  const closeCartHandler = () => {
+    setOpenCart(false);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header/>
+     {openCart && <Cart onCloseCart={closeCartHandler}/>}
+      <Header onOpenCart={openCartHandler}  />
       <main>
         <Meals/>
       </main>
